5d298c4dac5513fa8328db1b09786ccdfcd400db
Fannie <fannie.reyt@gmail.com>
2024-03-22 20:51:59 +0100

    Création Projet 6 - Pay my Buddy

---
+++ src/main/java/com/paymybuddy/applicationpaymybuddy/ApplicationPayMyBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.paymybuddy.applicationpaymybuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationPayMyBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationPayMyBuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paymybuddy/applicationpaymybuddy/ApplicationPayMyBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymybuddy.applicationpaymybuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationPayMyBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
