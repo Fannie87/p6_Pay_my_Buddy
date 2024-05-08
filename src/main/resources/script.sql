f324d76c1646e65dc1b1e18ba99568781b77deec
Fannie <fannie.reyt@gmail.com>
2024-05-08 21:16:21 +0200

    modif script sql

--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -1,45 +1,60 @@
+drop database pay_my_buddy;
+create database pay_my_buddy;
+use pay_my_buddy;
+
 DROP TABLE IF EXISTS connections;
 DROP TABLE IF EXISTS account;
 DROP TABLE IF EXISTS dbuser;
-
+DROP TABLE IF EXISTS trade;
+DROP TABLE IF EXISTS transaction;
 
 CREATE TABLE dbuser (
   id INT AUTO_INCREMENT  PRIMARY KEY,
   mail VARCHAR(250) NOT NULL,
   password VARCHAR(250) NOT null,
-  firstName VARCHAR(250),
-  lastName VARCHAR(250),
-  balance INT,
+  first_name VARCHAR(250) NOT null,
+  last_name VARCHAR(250) NOT null,
+  balance FLOAT default 0.0,
   CONSTRAINT UC_User UNIQUE (mail)
  );
 
-delete from dbuser;
-INSERT INTO dbuser (mail, password) VALUES 
-('user', '$2a$10$KpTU0U.XQznaqKR2C6AUVeN9YHL9FYKxJW/uIKYuGy3FGmdi6EI.i'),
-('admin', '$2y$10$kp1V7UYDEWn17WSK16UcmOnFd1mPFVF6UkLrOOCGtf24HOYt8p1iC'),
-('user1', '$2a$10$KpTU0U.XQznaqKR2C6AUVeN9YHL9FYKxJW/uIKYuGy3FGmdi6EI.i'),
-('user2', '$2a$10$KpTU0U.XQznaqKR2C6AUVeN9YHL9FYKxJW/uIKYuGy3FGmdi6EI.i'),
-('user3', '$2a$10$KpTU0U.XQznaqKR2C6AUVeN9YHL9FYKxJW/uIKYuGy3FGmdi6EI.i');
-
 CREATE TABLE account (
-  idAccount INT auto_increment PRIMARY KEY,
-  idUser INT,
-  FOREIGN KEY (idUser) REFERENCES dbuser(id),
+  id_account INT auto_increment PRIMARY KEY,
+  id_user INT NOT NULL,
+  FOREIGN KEY (id_user) REFERENCES dbuser(id),
   iban VARCHAR(250) NOT NULL,
   bic VARCHAR(250) NOT NULL,
-  nameAccount VARCHAR(250)
+  name_account VARCHAR(250)
   );
-
-INSERT INTO account (idUser, iban, bic, nameAccount) VALUES (1,'FRXXX','AGRI' , 'compte user');
-
-CREATE TABLE connections (
-  idUser INT,
-  idFriend INT,
-  FOREIGN KEY (idUser) REFERENCES dbuser(id),
-  FOREIGN KEY (idFriend) REFERENCES dbuser(id)
+ 
+ CREATE TABLE connections (
+  id_user INT,
+  id_friend INT,
+  FOREIGN KEY (id_user) REFERENCES dbuser(id),
+  FOREIGN KEY (id_friend) REFERENCES dbuser(id),
+  PRIMARY KEY(id_user, id_friend)
   );
+ 
+ create table transaction(
+	id_trade INT auto_increment primary KEY,
+	id_user INT,
+	id_friend INT,
+	description VARCHAR(250),
+	amount FLOAT not NULL,
+	FOREIGN KEY (id_user) REFERENCES dbuser(id)
+	);
 
-INSERT INTO connections (idUser, idFriend) VALUES (1,2), (1,3), (1,4);
 
+INSERT INTO dbuser (mail, password, first_name , last_name) VALUES 
+('user@gmail.com', '$2a$10$KpTU0U.XQznaqKR2C6AUVeN9YHL9FYKxJW/uIKYuGy3FGmdi6EI.i' 	, "user"  	, "0" ),
+('admin@gmail.com', '$2y$10$kp1V7UYDEWn17WSK16UcmOnFd1mPFVF6UkLrOOCGtf24HOYt8p1iC' 	, "admin"  	, "0" ),
+('user1@gmail.com', '$2a$10$KpTU0U.XQznaqKR2C6AUVeN9YHL9FYKxJW/uIKYuGy3FGmdi6EI.i'	, "user"  	, "1" ),
+('user2@gmail.com', '$2a$10$KpTU0U.XQznaqKR2C6AUVeN9YHL9FYKxJW/uIKYuGy3FGmdi6EI.i'	, "user"  	, "2" ),
+('user3@gmail.com', '$2a$10$KpTU0U.XQznaqKR2C6AUVeN9YHL9FYKxJW/uIKYuGy3FGmdi6EI.i' 	, "user"  	, "3" );
+INSERT INTO account (id_user, iban, bic, name_account) VALUES (1,'FRXXX','AGRI' , 'compte user');
+INSERT INTO connections (id_user, id_friend) VALUES (1,2), (1,3), (1,4);
+INSERT INTO transaction (id_user, id_friend, description, amount) VALUES 
+(1,2, 'Money send', 10.00),
+(1,2, 'Money send', 15.00),
+(1,3, 'Money send', 8.00);
 
-select * from connections;
